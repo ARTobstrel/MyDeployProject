dee0e632144fd1d2d2ab4273dc22702002d0c541
artem <artemfap@yandex.ru>
2017-09-18 22:53:41 +0300

    create app, init git

---
+++ article/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    class Meta:
+        """описывает название таблицы"""
+        db_table = "article"
+
+    article_title = models.CharField(max_length=200)
+    article_text = models.TextField()
+    article_date = models.DateTimeField()
