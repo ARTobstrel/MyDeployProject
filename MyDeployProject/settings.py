4ba94132e28b19bb9349d38e699db64b2a39598a
artem <artemfap@yandex.ru>
2017-09-25 00:06:41 +0300

    html and css

--- MyDeployProject/settings.py
+++ MyDeployProject/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'MyDeployProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "article/static"),
+]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "article/media")
 
 #TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
