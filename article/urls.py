6eed4e196ea220f8866a9011ba44264b96f54c0a
artem <artemfap@yandex.ru>
2017-12-09 00:02:21 +0300

    pagination, sources

--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
-from article.views import articles
+from article.views import *
+
 
 urlpatterns = [
-    url(r'^', articles, name='articles'),
+    url(r'^$', index, name='index'),
+    url(r'^articles/$', articles, name='articles'),
+    url(r'^articles/page/(\d+)/$', articles, name='articles_page'),
+    url(r'^article/(?P<art_id>\d+)/$', article, name='article'),
+
 ]
+
+
--- article/views.py
+++ article/views.py
@@ -4,11 +4,21 @@ from django.shortcuts import render_to_response
 from article.models import Article
 
 #My Views
-def articles(request, page_number=1):
+def index(request):
     """Основная страница"""
+    return render_to_response('index.html')
+
+def articles(request, page_number=1):
+    """Список всех статей"""
     all_articles = Article.objects.all()
     current_page = Paginator(all_articles, 9)
     return render_to_response('articles.html', {'articles':current_page.page(page_number),
                                                 'username':auth.get_user(request).username,
                                                 }
                               )
+
+def article(request, art_id=1):
+    """Выбранная статья"""
+    args = {}
+    args['article'] = Article.objects.get(id=art_id)
+    return render_to_response('article.html', args)
